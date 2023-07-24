bcd75fec9f4e2ecc789232ca6ad09c2c94993cef
Darien-B <93536644+Darien-B@users.noreply.github.com>
2023-07-23 22:26:41 -0600

    updated ai_agent.py to incorporate memory

--- ai_agent.py
+++ ai_agent.py
@@ -1,14 +1,28 @@
 import openai
 import os
+# Import the function to get memories from autogptblogDB.py
+from autogptblogDB import get_memories
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
+# Function to summarize memories
+def summarize_memories(memories):
+    # For now, just join the memories with a delimiter
+    return ' | '.join(memories)
+
 def generate_text(prompt):
+    # Get the memories from the database
+    memories = get_memories()
+    # Summarize the memories
+    summarized_memories = summarize_memories(memories)
+    # Include the summarized memories in the prompt
+    full_prompt = f"{summarized_memories} {prompt}"
+    
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt}
+            {"role": "user", "content": full_prompt}
         ]
     )
     return response.choices[0].message['content']
