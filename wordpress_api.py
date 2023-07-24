dfea656c7feb1462e4e6164ce21c52da7c2ab135
Darien-B <93536644+Darien-B@users.noreply.github.com>
2023-07-23 22:33:37 -0600

    added comment handling to wordpress_api.py

--- wordpress_api.py
+++ wordpress_api.py
@@ -25,3 +25,23 @@ def create_post(site_url, username, password, title, content):
         return response_json
     else:
         raise Exception('Failed to create post: {}'.format(response_json))
+
+def get_comments(site_url):
+    url = f'{site_url}/wp-json/wp/v2/comments'
+    response = requests.get(url)
+    response_json = response.json()
+    if response.status_code == 200:
+        return response_json
+    else:
+        raise Exception('Failed to get comments: {}'.format(response_json))
+
+def respond_to_comment(site_url, username, password, comment_id, response_text):
+    url = f'{site_url}/wp-json/wp/v2/comments/{comment_id}'
+    headers = {'Authorization': 'Basic ' + base64.b64encode(f'{username}:{password}'.encode()).decode()}
+    data = {'content': response_text}
+    response = requests.post(url, data=data, headers=headers)
+    response_json = response.json()
+    if response.status_code == 201:
+        return response_json
+    else:
+        raise Exception('Failed to respond to comment: {}'.format(response_json))
